e271cb075a09582720d0f018c06e0dfe4820bad7
muhammad-usman-outlier <muhammad.usman@engineering.outlier.org>
2022-05-19 12:17:54 -0600

    feat: updated action.yml and main.ts
    - action.yml (GitHub Action inputs, outputs and environment configurations)
    - main.ts (executes GitHub Action)

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,28 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as Core from '@actions/core'
+import {extractURLs} from './fetcher'
+import {getEmail, getPassword} from './constants'
+import {
+  createDeployment,
+  findDeploy,
+  findServer,
+  getContext,
+  logIn,
+  waitForDeploy
+} from './render'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    const [serviceId, preview]: any = await extractURLs()
+    Core.info('Starting Render Wait Action')
+    await logIn(getEmail, getPassword)
+    const context = getContext()
+    const serverId = await findServer(context, serviceId)
+    const render = await findDeploy(context, serverId)
+    const github = await createDeployment(context, render)
+    await waitForDeploy({render, github})
+    Core.setOutput('preview-url', preview)
+  } catch (error: any) {
+    Core.setFailed(error.message)
   }
 }
 
